7cb1ae26c88f6af7974d628ba13ec5251200a419
odevtheair <odevtheair@gmail.com>
2018-02-09 00:48:16 +0700

    Initial commit

---
+++ common/config/main.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        'urlManagerFrontEnd' => [                    // link backend to frontend
+            'class' => 'yii\web\urlManager',
+            //'baseUrl' => '../../frontend/web',            
+            //'baseUrl' => '../../frontend/web/index.php?r=',
+            
+            'baseUrl' => '../../frontend/web/',
+            'scriptUrl'=>'../../frontend/web/index.php',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+        'urlManagerBackEnd' => [                    // link backend to frontend
+            'class' => 'yii\web\urlManager',
+            //'baseUrl' => '../../backend/web',
+            
+            'baseUrl' => '../../backend/web/',
+            'scriptUrl'=>'../../backend/web/index.php',
+            
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
+    ],
+
+    //---
+    'modules' => [
+        'gridview' => [
+               'class' => '\kartik\grid\Module'
+        ],
+    ]
+       //...
+];
